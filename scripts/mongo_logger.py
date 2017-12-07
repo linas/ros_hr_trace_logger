df92521cade5f22c063a4a07eca59c1dba69e073
Wenwei Huang <dut.hww@gmail.com>
2017-12-07 18:40:59 +0800

    Initial commit

---
+++ scripts/mongo_logger.py
@@ -0,0 +1,59 @@
+import datetime as dt
+import traceback
+import rospy
+from chatbot.db import get_mongo_client
+from hr_msgs.msg import TTS
+from hr_msgs.msg import EmotionState
+from hr_msgs.msg import SetGesture
+
+ROBOT_NAME = os.environ.get('NAME', 'default')
+
+class MongoLogger(object):
+    def __init__(self):
+        try:
+            self.mongoclient = get_mongo_client()
+        except Exception as ex:
+            self.mongoclient = None
+        self.run_id = rospy.get_param('/run_id', '')
+
+        rospy.Subscriber('/blender_api/set_emotion_state', EmotionState, self.log_emotion)
+        rospy.Subscriber('/blender_api/set_gesture', SetGesture, self.log_gesture)
+
+    def _log(self, collection, record):
+        try:
+            result = collection.insert_one(record)
+            logger.info("Added record to mongodb")
+        except Exception as ex:
+            self.mongoclient.client = None
+            logger.error(traceback.format_exc())
+            logger.warn("Deactivate mongodb")
+
+    def log_emotion(self, msg):
+        record = {
+            'Datetime': dt.datetime.now(),
+            'RunID': self.run_id,
+            'Name': msg.name,
+            'Magnitude': msg.magnitude,
+            'Duration': msg.duration,
+        }
+        if self.mongoclient is not None and self.mongoclient.client is not None:
+            collection = self.mongoclient.client[ROBOT_NAME]['blender']['emotion_state']
+            self._log(collection, record)
+
+    def log_gesture(self, msg):
+        record = {
+            'Datetime': dt.datetime.now(),
+            'RunID': self.run_id,
+            'Name': msg.name,
+            'Repeat': msg.repeat,
+            'Speed': msg.speed,
+            'Magnitude': msg.magnitude,
+        }
+        if self.mongoclient is not None and self.mongoclient.client is not None:
+            collection = self.mongoclient.client[ROBOT_NAME]['blender']['gesture']
+            self._log(collection, record)
+
+if __name__ == '__main__':
+    rospy.init_node('mongo_logger')
+    logger = MongoLogger()
+    rospy.spin()
